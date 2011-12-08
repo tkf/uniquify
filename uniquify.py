1623bae54b00545c1ebe664c9f1a7e41d14f77e2
Takafumi Arakaki <aka.tkf@gmail.com>
2011-12-08 16:47:38 +0100

    Basic functions uniqname and uniqpath work

---
+++ uniquify.py
@@ -0,0 +1,144 @@
+import os
+
+
+def uniqname(names, sep=None, skip='...'):
+    """
+    Generate unique names from a list of strings
+
+    >>> uniqname(['aa', 'ab'], skip='')
+    ['a', 'b']
+    >>> uniqname(['aac', 'abc'], skip='')
+    ['a', 'b']
+    >>> uniqname(['aac', 'abc'])
+    ['aac', 'abc']
+    >>> uniqname(['aaxxxxc', 'abxxxxb', 'abxxxxc'])
+    ['aa...c', 'ab...b', 'ab...c']
+    >>> uniqname(['aa|c|c|de', 'ab|c|c|dd', 'ab|c|c|de'], sep='|')
+    ['aa|...|de', 'ab|...|dd', 'ab|...|de']
+    >>> uniqname(['aa|c|de', 'ab|c|dd', 'ab|c|de'], sep='|')
+    ['aa|c|de', 'ab|c|dd', 'ab|c|de']
+
+    """
+    if sep is None:
+        lol = list(list(n) for n in names)
+        sep = ''
+    else:
+        lol = list(n.split(sep) for n in names)
+    chunks = _get_chunks(lol)
+    return list(_get_unique_name(n, chunks, sep, skip) for n in lol)
+
+
+def uniqpath(paths, skip='...'):
+    """
+    Generate unique names from a list of file paths
+
+    >>> uniqpath(['a/a', 'a/b'], skip='*')
+    ['*/a', '*/b']
+    >>> uniqpath(['a/a/c', 'a/b/c'], skip='*')
+    ['*/a/*', '*/b/*']
+    >>> uniqpath(['a/ac', 'a/bc'], skip='*')
+    ['*/ac', '*/bc']
+
+    """
+    return uniqname(paths, os.path.sep, skip)
+
+
+def _get_unique_name(name, chunks, sep, skip):
+    """
+    Generate unique name; avoid skipping if it makes name longer
+
+    >>> chunks = ([(0, 5), (5, 10), (10, 15)], [True, False, True])
+    >>> _get_unique_name(list('aaaaa*****ccccc'), chunks, '', '...')
+    'aaaaa...ccccc'
+    >>> chunks = ([(0, 1), (1, 2), (2, 3)], [True, False, True])
+    >>> _get_unique_name(['a', '*', 'c'], chunks, '/', '...')
+    'a/*/c'
+    >>> _get_unique_name(['aaaaa', '*****', 'ccccc'], chunks, '/', '...')
+    'aaaaa/.../ccccc'
+
+    """
+    skipwidth = len(skip)
+    newname = []
+    for ((start, stop), diff) in zip(*chunks):
+        subname = sep.join(name[start:stop])
+        if diff or len(subname) < skipwidth:
+            newname.append(subname)
+        else:
+            newname.append(skip)
+    return sep.join(newname)
+
+
+def _get_chunks(lol):
+    """
+    Returns common and different "chunks" of the list in the list (``lol``)
+
+
+    chunks : (ranges, diffs)
+        Common and different chunks
+
+        ranges : [(start, stop)] --- list of start-stop pairs
+            Definition of the ranges
+
+            start : int
+                The first index of the range
+
+            stop : int
+                The last + 1 index of the range
+
+        diffs : [bool] --- list of bools
+            True if the corresponding element in ``ranges`` is
+            different, otherwise False.
+
+    Examples:
+
+    >>> _get_chunks([[1, 2, 3],
+    ...              [1, 2, 2]])
+    ([(0, 2), (2, 3)], [False, True])
+    >>> _get_chunks([[1, 2, 1, 2, 3, 3, 1, 2],
+    ...              [1, 2, 1, 2, 4, 4, 1, 2]])
+    ([(0, 4), (4, 6), (6, 8)], [False, True, False])
+
+    """
+    ranges = []
+    diffs = []
+    rawdiffs = _diff_list(lol)
+    start = 0
+    for (i, (d0, d1)) in enumerate(zip(rawdiffs[:-1], rawdiffs[1:])):
+        if d0 != d1:
+            ranges.append((start, i + 1))
+            diffs.append(d0)
+            start = i + 1
+    ranges.append((start, len(rawdiffs)))
+    diffs.append(d1)
+    return (ranges, diffs)
+
+
+def _diff_list(lol):
+    """
+    Find the different part in `lol` (list of list)
+
+    >>> _diff_list([[1, 2, 3],
+    ...             [1, 2, 2]])
+    [False, False, True]
+    >>> _diff_list([[1, 2, 3],
+    ...             [1, 2, 2],
+    ...             [1, 1, 2]])
+    [False, True, True]
+    >>> _diff_list([[1, 2, 3],
+    ...             [1, 2]])
+    [False, False, True]
+
+    """
+    if  len(lol) < 2:
+        raise ValueError('Need at least 2 list. {0} given'.format(len(lol)))
+
+    ls0 = lol[0]
+    ls0len = len(ls0)
+    maxlen = max(len(ls) for ls in lol)
+    indices = range(maxlen)
+    diff = [False] * maxlen
+    for ls in lol[1:]:
+        for i in indices:
+            if ls0len <= i or len(ls) <= i or ls0[i] != ls[i]:
+                diff[i] = True
+    return diff
