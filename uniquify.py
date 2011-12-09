cd7ccfcc2acc4f9b3c20adb35129c14a4929973b
Takafumi Arakaki <aka.tkf@gmail.com>
2011-12-09 19:03:26 +0100

    Added install and improved examples in README.rst

--- uniquify.py
+++ uniquify.py
@@ -7,6 +7,8 @@
 Uniquify - get unique, short and easy-to-read names and paths
 =============================================================
 
+Examples
+--------
 
 Shorten names/paths by extracting non-common parts:
 
@@ -15,10 +17,10 @@ Shorten names/paths by extracting non-common parts:
 ...            '__common_part___ijk___common_part__',
 ...            '__common_part___xyz___common_part__'])
 ['abc', 'ijk', 'xyz']
->>> shortpath(['some/long/path/___/abc/___/___/',
-...            'some/long/path/___/ijk/___/___/',
-...            'some/long/path/___/xyz/___/___/'])
-['abc', 'ijk', 'xyz']
+>>> shortpath(['some/long/path/___/alpha/___/___/',
+...            'some/long/path/___/beta/___/___/',
+...            'some/long/path/___/gamma/___/___/'])
+['alpha', 'beta', 'gamma']
 
 
 Convert common parts into skip marks:
@@ -28,10 +30,23 @@ Convert common parts into skip marks:
 ...                 'ij__common_part___k',
 ...                 'xy__common_part___z'])
 ['ab...c', 'ij...k', 'xy...z']
->>> skipcommonpath(['ab/common/path/c',
-...                 'ij/common/path/k',
-...                 'xy/common/path/z'])
-['ab/.../c', 'ij/.../k', 'xy/.../z']
+>>> skipcommonpath(['alpha/common/path/beta',
+...                 'epsilon/common/path/delta',
+...                 'phi/common/path/psi'])
+['alpha/.../beta', 'epsilon/.../delta', 'phi/.../psi']
+
+
+Install
+-------
+
+Install it from pypi_
+
+::
+
+  pip install uniquify
+  # easy_install uniquify
+
+.. _pypi: http://pypi.python.org/pypi/uniquify/
 """
 # [[[end]]]
 
